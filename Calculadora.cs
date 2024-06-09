4b43e1f77d005c02c961ea0b76a8ed28c0821bf3
ElZorroAs <fedeaaatucuman@gmail.com>
2024-06-08 23:00:50 -0300

    primer commit

---
+++ Calculadora.cs
@@ -0,0 +1,38 @@
+﻿namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    private double dato = 0;
+
+    public void Sumar(double termino)
+    {
+        dato = dato + termino;
+    }
+
+    public void Restar(double termino)
+    {
+        dato = dato - termino;
+    }
+
+    public void Multiplicar(double termino) {
+        dato= dato * termino;
+     }
+
+    public void Dividir(double termino) {
+        if (termino != 0)
+        {
+            dato = dato/termino;
+        }else{
+            Console.WriteLine("error papu es 0");
+        }
+     }
+
+    public void Limpiar() {
+        dato=0;
+     }
+
+    public double resultado
+    {
+        get { return dato; }
+    }
+}
